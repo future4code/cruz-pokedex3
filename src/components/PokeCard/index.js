a6e9859a7ddebbeb5318b60cb2682bfedef9c3ed
Giselle Rosa <gisellenrosa@gmail.com>
2021-04-22 11:28:24 -0300

    add global state

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Example  from '../../images/Error.png'
 import { Container, Img } from './styles';
+import BaseUrl from '../../constants/BaseUrl'
 
 
 export default function PokeCard() {
+
+
   return (
       <div>
           <Container>
             <Img src={Example} alt={"exemplo da foto do pokemon"}/>
             <h1> Aqui vao ficar os cards de pokemons :O</h1>
+
+       
           </Container>
       </div>
   );
---
+++ src/constants/BaseUrl.js
@@ -0,0 +1,2 @@
+export const BaseUrl =
+`https://pokeapi.co/api/v2/pokemon`;
